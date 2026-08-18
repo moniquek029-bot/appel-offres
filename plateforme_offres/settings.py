d422e42a0476885918f3b9e3bd97a41193cefe2a
Kabre Monique <moniquek029@gmail.com>
2026-08-18 17:06:53 +0100

    Fix: Suppression définitive de pymysql pour Render

--- plateforme_offres/settings.py
+++ plateforme_offres/settings.py
@@ -5,7 +5,7 @@ import django.db.backends.mysql.base
 from celery.schedules import crontab
 from dotenv import load_dotenv
 import dj_database_url
-pymysql.install_as_MySQLdb()
+#pymysql.install_as_MySQLdb()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
